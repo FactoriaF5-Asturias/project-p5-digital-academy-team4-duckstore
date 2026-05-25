ccdf3fa0f180627ee8abed882c5f01dbdcc91397
iulian640 <juliantimofei15@gmail.com>
2026-05-25 09:43:21 +0200

    feat. Organizado array de patos

--- data/ducks.js
+++ data/ducks.js
@@ -1,4 +1,4 @@
-const ducks = [
+export const ducks = [
     { id: 'duck-classic', name: 'Pato clásico', description: 'Un pato clásico de plástico', price: 10.00, img: '/assets/duck-classic.jpg', alt: 'Pato clásico', category: 'Clásico' },
     { id: 'duck-astro', name: 'Pato astronauta', description: 'Un pato astronauta muy majete', price: 19.69, img: '/assets/duck-astro.jpg', alt: 'Pato astronauta', category: 'Aventurero' },
     { id: 'duck-evil', name: 'Pato maleante ', description: 'Un pato astronauta muy majete', price: 19.69, img: '/assets/duck-evil.jpg', alt: 'Pato maleante', category: 'Villano' },
