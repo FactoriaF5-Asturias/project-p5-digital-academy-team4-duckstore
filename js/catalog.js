982b83ff871c4342a3f8135a22040de319501c3d
iulian640 <juliantimofei15@gmail.com>
2026-05-26 16:56:31 +0200

    feat. Arreglados errors de sintáxis en catalog j

--- js/catalog.js
+++ js/catalog.js
@@ -1,12 +1,22 @@
 import {ducks} from '../data/ducks.js';
+
 const catalogoContainer = document.getElementById('catalogo');
+
+const cards = ducks.map(duck =>`
 <article class="patos" data-id="${duck.id}">
+    <img src="${duck.img}" alt="${duck.alt}">
 <div class="info">
-    <h2 class="titulo">${ducks.name}</h2>
-    <p class="descipcion">${ducks.description}</p>
-    <span class="precio">${duck.price.toFixed(2).replace('.', ','){duck.price.toFixed(2).replace('.', ',')}€</span>
+    <h2 class="titulo">${duck.name}</h2>
+    <p class="descripcion">${duck.description}</p>
+    <span class="precio">${duck.price.toFixed(2).replace('.', ',')}€</span>
     <button class="add-to-cart" data-id="${duck.id}">Añadir al carrito</button>
+    </div>
+  
+ 
 
-</div>
 
-</article>>
+</article>
+
+  `)
+
+  catalogoContainer.innerHTML = cards. join('')
